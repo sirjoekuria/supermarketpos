889a175d7a82b66d7e8d961c447d53d568feccf5
Joseph Kuria <kuriajoe85@gmail.com>
2026-06-05 01:04:41 +0300

    perf: remove heavy webpack polling in dev mode

--- next.config.js
+++ next.config.js
@@ -58,16 +58,6 @@ const nextConfig = {
     },
   } : {}),
 
-  // Webpack configuration
-  webpack: (config, { dev }) => {
-    if (dev) {
-      config.watchOptions = {
-        poll: 1000,
-        aggregateTimeout: 300,
-      };
-    }
-    return config;
-  },
 };
 
 module.exports = nextConfig;
